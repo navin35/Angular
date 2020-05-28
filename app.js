802d43bf6cc43edd30c88d9f01d3dffe190e87e4
navin35 <65673849+navin35@users.noreply.github.com>
2020-05-27 19:53:49 -0700

    Add files via upload
    assignment1

---
+++ app.js
@@ -0,0 +1,36 @@
+(
+    function () {
+        angular.module('firstApp',[])
+        .controller('firstAppController', ctrl );
+        ctrl.$inject = ['$scope'];
+        function ctrl($scope) {
+            $scope.items = '';
+            $scope.message = '';
+           
+
+            $scope.check = function (){
+                let arrayOfItems = $scope.items.split(',');
+                let counter = 0;
+
+                arrayOfItems.forEach((item) => {
+                    if (item != false) {
+                        counter++;
+                    }
+                });
+
+                if (counter > 0 && counter < 4){
+                    $scope.message = 'Enjoy!';
+                    
+                }else if(counter >= 4){
+                    $scope.message = 'Too much!';
+                    
+                    
+                }else{
+                    $scope.message = 'Please enter data first';
+                    
+                }
+            }
+        }
+        
+    }
+)()
